d0b9804a5c0b7747b4ceec8bf78612f3aa76470b
tea <ducthaidev@gmail.com>
2025-04-10 06:16:04 +0700

    WIP: create account

--- src/main/java/com/digitalcashvault/engine/account/account.java
+++
@@ -1,6 +0,0 @@
-import jakarta.persistence.Id;
-
-public class account {
-  @Id
-  public Long id;
-}
--- src/main/java/com/digitalcashvault/engine/account/controller.java
+++ src/main/java/com/digitalcashvault/engine/account/controller.java
@@ -1,3 +1,5 @@
+package com.digitalcashvault.engine.account;
+
 public class controller {
   
 }
